af019b31df21d59e5ab71e19ec097d15bc911e57
Ipan Zelvdsk <116007299+zelvdsk@users.noreply.github.com>
2023-08-08 20:20:47 +0700

    Update setup.py
    Add other browser and get random browser

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     author='Errucha', 
     name='random-ua', 
-    version='0.1',
+    version='0.2',
     entry_points={"console_scripts": ["scraping=random_ua:ugent"]}, 
     packages=['random_ua'],
     install_requires=[
