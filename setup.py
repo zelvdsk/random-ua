822a29960d571548560dc5dbad8c14abb515c9ca
Ipan Zelvdsk <116007299+zelvdsk@users.noreply.github.com>
2023-08-08 15:39:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 
 setup(
     author='Errucha', 
-    name='random-ua',
-    descriptions='Alat untuk mengambil useragent secara random dari website', 
+    name='random-ua', 
     version='0.1',
     entry_points={"console_scripts": ["random-ua:random-ua.main:scraping"]}, 
     py_modules=['random-ua'],
